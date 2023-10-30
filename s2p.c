ccb6b1846ef087ffeed4f8dbd7a0435f283d977c
Arda Eden <ardaeden@gmail.com>
2023-10-30 12:04:45 +0300

    A great achievement

--- s2p.c
+++ s2p.c
@@ -6,19 +6,28 @@ static t_class *s2p_class;
 
 typedef struct _s2p {
   t_object  x_obj;
+  t_atom outList[5];
+  t_int index;
+  t_outlet *l_out;
 } t_s2p;
 
 
 
 void s2p_float(t_s2p *x, t_floatarg f)
 {
-  outlet_float(x->x_obj.ob_outlet, f);
+  SETFLOAT(x->outList+x->index, f);
+  x->index++;
+  if (x->index > 4) {
+    x->index = 0;
+    outlet_list(x->l_out, 0, 5, x->outList);
+  }
 }
 
 void *s2p_new(void)
 {
   t_s2p *x = (t_s2p *)pd_new(s2p_class);
-  outlet_new(&x->x_obj, &s_float);
+  x->index = 0;
+  x->l_out=outlet_new(&x->x_obj, gensym("list"));
   return (void *)x;
 }
 

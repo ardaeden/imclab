abb4a6cc3531a25eedd17cbd564d33992e47b9d3
Arda Eden <ardaeden@gmail.com>
2023-11-05 15:07:04 +0300

    Changes made

--- s2p.c
+++ s2p.c
@@ -4,9 +4,9 @@ static t_class *s2p_class;
 
 typedef struct _s2p {
   t_object  x_obj;
-  t_atom outList[5];
+  t_atom outList[256];
   t_int index;
-  t_float limit;
+  t_float range;
   t_outlet *l_out;
 } t_s2p;
 
@@ -14,28 +14,29 @@ void s2p_float(t_s2p *x, t_floatarg f)
 {
   SETFLOAT(x->outList+x->index, f);
   x->index++;
-  if (x->index > 4) {
+  if (x->index >= x->range) {
     x->index = 0;
-    outlet_list(x->l_out, 0, 5, x->outList);
+    outlet_list(x->l_out, 0, x->range, x->outList);
   }
 }
 
-void *s2p_new(void)
+void *s2p_new(t_floatarg f)
 {
   t_s2p *x = (t_s2p *)pd_new(s2p_class);
-  x->index = 0;
-  floatinlet_new(&x->x_obj, &x->limit);
+  x->range = f;
+  x->index = 0.0;
+  //floatinlet_new(&x->x_obj, &x->range);
   x->l_out=outlet_new(&x->x_obj, &s_list);
   return (void *)x;
 }
 
 void s2p_setup(void) {
-  /* create a new class */
   s2p_class = class_new(gensym("s2p"),
-                               (t_newmethod)s2p_new,
-                               0,
-                               sizeof(t_s2p),
-                               CLASS_DEFAULT,
-                               0);
+                        (t_newmethod)s2p_new,
+                        0,
+
+                        sizeof(t_s2p),
+                        CLASS_DEFAULT,
+			A_DEFFLOAT, 0);
   class_addfloat(s2p_class, s2p_float);
 }
